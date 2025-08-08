446a803045110f523c0553722e282366f5415242
bhumi2007 <bs8574508@gmail.com>
2025-08-08 23:33:48 +0530

    Create expense_tracker.py

---
+++ expense_tracker.py
@@ -0,0 +1,10 @@
+exp_name = input("Enter the name of the expense: ")
+exp_date = input("Enter the date of the expense: ")
+amt = float(input("Enter the amount of money paid: "))
+des = input("Enter the description of the product: ")
+exp_details={
+ "name":exp_name,
+ "date":exp_date, 
+ "amt":amt,
+ "des":des
+}
